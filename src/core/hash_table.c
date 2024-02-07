95f8a1eb70f6606a3b29d71877d34f86bb548c74
Kevin d'Orange <kevin.dorange@protonmail.com>
2024-02-07 15:15:45 +0100

    Start HashTable structure

---
+++ src/core/hash_table.c
@@ -0,0 +1,60 @@
+#include <banjo/hash_table.h>
+#include <core/errors.h>
+#include <core/memory.h>
+
+BjResult bjCreateHashTable(
+    const BjHashTableCreateInfo* pCreateInfo,
+    BjHashTable* pInstance
+) {
+    bjExpectValue(pInstance, BJ_NULL_OUTPUT_HANDLE);
+    bjExpectValue(pCreateInfo->elem_size, BJ_INVALID_PARAMETER);
+
+    BjHashTable htable = bjNewStruct(BjHashTable, pCreateInfo->pAllocator);
+
+    BjResult result = bjInitHashTable(pCreateInfo, htable);
+    if(result == BJ_SUCCESS) {
+        *pInstance = htable;
+    } else {
+        bjFree(htable, pCreateInfo->pAllocator);
+    }
+
+    return BJ_SUCCESS;
+}
+
+BjResult bjInitHashTable(
+    const BjHashTableCreateInfo* pCreateInfo,
+    BjHashTable                  pInstance
+) {
+    bjExpectValue(pCreateInfo, BJ_NULL_CREATE_INFO);
+
+    pInstance->pAllocator = pCreateInfo->pAllocator;
+    pInstance->elem_size  = pCreateInfo->elem_size;
+
+    return BJ_SUCCESS;
+}
+
+BjResult bjResetHashTable(
+    BjHashTable htable
+) {
+    BjResult res      = bjClearHashTable(htable);
+    htable->elem_size = 0;
+    return res;
+}
+
+BjResult bjDestroyHashTable(
+    BjHashTable htable
+) {
+    bjExpectValue(htable, BJ_NULL_PARAMETER);
+    BjResult res =  bjResetHashTable(htable);
+    bjFree(htable, htable->pAllocator);
+    return res;
+}
+
+BANJO_EXPORT BjResult bjClearHashTable(
+    BjHashTable htable
+) {
+    bjExpectValue(htable, BJ_NULL_PARAMETER);
+
+    return BJ_SUCCESS;
+}
+
---
+++ test/BjHashTable.c
@@ -0,0 +1,37 @@
+#include "test.h"
+#include "custom_allocators.h"
+
+#include <banjo/hash_table.h>
+
+
+TEST_CASE(zero_initialization) {
+    BjHashTable htable;
+
+    BjResult result = bjCreateHashTable(&(BjHashTableCreateInfo){.elem_size=1}, &htable);
+    REQUIRE_EQ(result, BJ_SUCCESS);
+
+    REQUIRE_EQ(htable->pAllocator, 0);
+
+    bjDestroyHashTable(htable);
+}
+
+TEST_CASE(custom_allocator) {
+    BjAllocationCallbacks allocator = mock_allocators();
+    BjHashTable htable;
+
+    BjResult result = bjCreateHashTable(&(BjHashTableCreateInfo){ .pAllocator = &allocator, .elem_size=1 },&htable);
+    REQUIRE_EQ(result, BJ_SUCCESS);
+
+    REQUIRE(htable->pAllocator != 0);
+
+    bjDestroyHashTable(htable);
+}
+
+int main(int argc, char* argv[]) {
+    BEGIN_TESTS(argc, argv);
+
+    RUN_TEST(zero_initialization);
+    RUN_TEST(custom_allocator);
+
+    END_TESTS();
+}
