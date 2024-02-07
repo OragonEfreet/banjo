2cb62ff269f6db48b6647fd9939d2776377e03b1
Kevin d'Orange <kevin.dorange@protonmail.com>
2024-02-07 01:24:21 +0100

    Add Init/Reset mechanism

--- src/core/errors.h
+++ src/core/errors.h
@@ -5,11 +5,17 @@
 #endif
 
 #ifdef NDEBUG
-#   define bjExpectValue(ptr, result) if(!ptr) {return result}
-#   define bjExpectNull(ptr, result) if(!!ptr) {return result}
-#   define bjExpect(cond, result) if(!(cond))  {return result}
+#   ifdef BANJO_PEDANTIC
+#      define bjExpectValue(ptr, result) if(!ptr) {return result}
+#      define bjExpectNull(ptr, result)  if(!!ptr) {return result}
+#      define bjExpect(cond, result)     if(!(cond))  {return result}
+#   else
+#      define bjExpectValue(...)
+#      define bjExpectNull(...)
+#      define bjExpect(...)
+#   endif
 #else
 #   define bjExpectValue(ptr, result) assert(ptr)
-#   define bjExpectNull(ptr, result) assert(!ptr)
-#   define bjExpect(cond, result) assert(cond)
+#   define bjExpectNull(ptr, result)  assert(!ptr)
+#   define bjExpect(cond, result)     assert(cond)
 #endif
