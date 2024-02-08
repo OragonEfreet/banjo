50fee56ee95b0579d6a46971a4310915489cf64e
Kevin d'Orange <kevin.dorange@protonmail.com>
2024-02-08 14:58:47 +0100

    Add forward_list and remove glfw dep

---
+++ inc/banjo/forward_list.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <banjo/core.h>
+#include <banjo/memory.h>
+
+typedef struct BjForwardListEntry {
+    void* value;
+    struct BjForwardListEntry* pNext;
+} BjForwardListEntry;
+
+typedef struct BjForwardList_T {
+    BjAllocationCallbacks*     pAllocator;
+    usize                      elem_size;
+    struct BjForwardListEntry* pFirstEntry;
+} BjForwardList_T;
+
+BJ_DEFINE_HANDLE(BjForwardList);
+
+typedef struct BjForwardListCreateInfo {
+    BjAllocationCallbacks* pAllocator;
+    usize                  elem_size;
+} BjForwardListCreateInfo;
+
+BANJO_EXPORT BjResult bjCreateForwardList(
+    const BjForwardListCreateInfo* pCreateInfo,
+    BjForwardList*                 pInstance
+);
+
+BANJO_EXPORT BjResult bjInitForwardList(
+    const BjForwardListCreateInfo* pCreateInfo,
+    BjForwardList                  pInstance
+);
+
+BANJO_EXPORT BjResult bjResetForwardList(
+    BjForwardList list
+);
+
+BANJO_EXPORT BjResult bjDestroyForwardList(
+    BjForwardList list
+);
+
+
+
+
+
