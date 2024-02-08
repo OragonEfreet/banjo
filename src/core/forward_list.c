83be6f75eda5f0d3afaf03cd2dd181d0deb1162e
Kevin d'Orange <kdorange@techsoft3d.com>
2024-02-08 14:58:47 +0100

    Add forward_list and remove glfw dep

---
+++ src/core/forward_list.c
@@ -0,0 +1,52 @@
+#include <banjo/forward_list.h>
+#include <core/errors.h>
+#include <core/memory.h>
+
+BjResult bjCreateForwardList(
+    const BjForwardListCreateInfo* pCreateInfo,
+    BjForwardList* pInstance
+) {
+    bjExpectValue(pInstance, BJ_NULL_OUTPUT_HANDLE);
+    bjExpectValue(pCreateInfo->elem_size, BJ_INVALID_PARAMETER);
+
+    BjForwardList list     = bjNewStruct(BjForwardList, pCreateInfo->pAllocator);
+
+    BjResult result = bjInitForwardList(pCreateInfo, list);
+    if(result == BJ_SUCCESS) {
+        *pInstance = list;
+    } else {
+        bjFree(list, pCreateInfo->pAllocator);
+    }
+
+    return BJ_SUCCESS;
+}
+
+BjResult bjInitForwardList(
+    const BjForwardListCreateInfo* pCreateInfo,
+    BjForwardList                  pInstance
+) {
+    bjExpectValue(pCreateInfo, BJ_NULL_CREATE_INFO);
+
+    pInstance->pAllocator  = pCreateInfo->pAllocator;
+    pInstance->elem_size   = pCreateInfo->elem_size;
+    pInstance->pFirstEntry = 0;
+
+    return BJ_SUCCESS;
+}
+
+BjResult bjResetForwardList(
+    BjForwardList list
+) {
+    bjExpectValue(list, BJ_NULL_PARAMETER);
+    return BJ_SUCCESS;
+}
+
+BjResult bjDestroyForwardList(
+    BjForwardList list
+) {
+    bjExpectValue(list, BJ_NULL_PARAMETER);
+    BjResult res = bjResetForwardList(list);
+    bjFree(list, list->pAllocator);
+    return res;
+}
+
