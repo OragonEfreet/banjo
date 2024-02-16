26ef4e0b3e4a1518cc63421074495c942015cba1
Kevin d'Orange <kdorange@techsoft3d.com>
2024-02-16 21:24:42 +0100

    Progress on array

--- inc/banjo/array.h
+++ inc/banjo/array.h
@@ -25,6 +25,27 @@ BANJO_EXPORT void bj_array_reserve(
     usize   capacity
 );
 
+BANJO_EXPORT void bj_array_push(
+    BjArray array,
+    const void* value
+);
+
+BANJO_EXPORT void bj_array_pop(
+    BjArray array
+);
+
+BANJO_EXPORT void* bj_array_at(
+    const BjArray array,
+    usize   at
+);
+
+BANJO_EXPORT void* bj_array_data(
+    const BjArray array
+);
+
+BANJO_EXPORT usize bj_array_count(
+    const BjArray array
+);
 
 
 
--- test/tdd_BjArray.c
+++ test/tdd_BjArray.c
@@ -2,7 +2,7 @@
 
 #include <array.h>
 
-TEST_CASE_ARGS(zero_initialization, {}) {
+TEST_CASE(zero_initialization) {
 
     BjArray array = bj_array_create(&(BjArrayInfo){.value_size=1}, 0);
     REQUIRE_VALUE(array);
@@ -15,7 +15,7 @@ TEST_CASE_ARGS(zero_initialization, {}) {
     bj_array_destroy(array);
 }
 
-TEST_CASE_ARGS(init_explicit_capacity, {}) {
+TEST_CASE(init_explicit_capacity) {
 
     BjArray array = bj_array_create(&(BjArrayInfo){.value_size=1}, 0);
     REQUIRE_VALUE(array);
@@ -23,11 +23,46 @@ TEST_CASE_ARGS(init_explicit_capacity, {}) {
     bj_array_destroy(array);
 }
 
+typedef struct {
+    int i;
+} value_type;
+
+static value_type values[] = {
+    {.i = 42},
+    {.i = 420},
+};
+static usize n_values = sizeof(values) / sizeof(values[0]);
+
+TEST_CASE(insertions) {
+
+    BjArray array = bj_array_create(&(BjArrayInfo){.value_size=sizeof(value_type)}, 0);
+    CHECK_VALUE(array);
+
+    CHECK_EQ(array->capacity, 0);
+    CHECK_EQ(array->count, 0);
+    CHECK_EQ(array->p_allocator, 0);
+    CHECK_EQ(array->p_data, 0);
+
+    for(usize i = 0 ; i < n_values ; ++i) {
+        bj_array_push(array, &values[i]);
+        REQUIRE_EQ(bj_array_count(array), i + 1);
+        REQUIRE_EQ(array->count, i + 1);
+        
+        value_type* got = bj_array_at(array, i);
+        REQUIRE_VALUE(got);
+        REQUIRE_EQ(got->i, values[i].i);
+    }
+
+    bj_array_destroy(array);
+}
+
+
 int main(int argc, char* argv[]) {
     BEGIN_TESTS(argc, argv);
 
     RUN_TEST(zero_initialization);
     RUN_TEST(init_explicit_capacity);
+    RUN_TEST(insertions);
 
     END_TESTS();
 }
