e35da4e4f99e6cd410391fa8d0f99c0f08f0af41
Kevin d'Orange <kdorange@techsoft3d.com>
2024-02-19 15:03:14 +0100

    Start Some Docs

--- inc/banjo/world.h
+++ inc/banjo/world.h
@@ -1,29 +1,35 @@
+/// \file world.h
+/// \brief Main structure for Entity-Component-System object.
 #pragma once
 
 #include <banjo/api.h>
 #include <banjo/memory.h>
 
-typedef usize entity_id;
-
+/// Handle type for World instances.
 BJ_DEFINE_HANDLE(BjWorld);
 
+/// Creation info for a new instance of BjWorld.
+/// \see BjWorld
 typedef struct BjWorldInfo {
-    int rfu;
+    int rfu; //!< Unused
 } BjWorldInfo;
 
+/// Create a new instance of \ref BjWorld.
+///
+/// The caller owns the returned object.
+/// To free it properly, use \ref bj_world_destroy.
+///
+/// \return A new \ref BjWorld.
 BANJO_EXPORT BjWorld bj_world_create(
-    const BjWorldInfo*           p_info,
-    const BjAllocationCallbacks* p_allocator
+    const BjWorldInfo*           p_info,        ///< The creation configuration.
+    const BjAllocationCallbacks* p_allocator    ///< Custom allocator. Can be _0_.
 );
 
+/// Destroy an instance of \ref BjWorld.
 BANJO_EXPORT void bj_world_destroy(
-    BjWorld world
+    BjWorld world                               //!< The World instance to destroy.
 );
 
-// Tableau de component id -> Signature? Has to be sorted
-// Archetype -> Tableau de signatures+entity ID qui ont cette signature
-// Map EntityID -> Archetype?
-// Map Signature->Archetype?
 
 
 
