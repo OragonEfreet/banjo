0da0be5cd5a472902d690f23bd9f14d8737deaac
Kevin d'Orange <kdorange@techsoft3d.com>
2024-05-08 13:24:43 +0200

    .

--- inc/banjo/byte_stream.h
+++ inc/banjo/byte_stream.h
@@ -14,6 +14,7 @@ typedef struct bj_byte_stream_t bj_byte_stream; ///< Typedef for the bj_byte_str
 
 /// \brief The internal data structure for the \ref bj_byte_stream type.
 struct bj_byte_stream_t {
+    bool    weak;       //!< True is memory buffer is not managed by the object
     u8*     p_data;     //!< Array of stream data
     usize   len;        //!< Size of the stream (in bytes)
     usize   position;   //!< Current position within the stream
@@ -21,13 +22,27 @@ struct bj_byte_stream_t {
 
 BANJO_EXPORT bj_byte_stream* bj_byte_stream_init_default(
     bj_byte_stream* p_instance,
-    usize size
+    usize           size
 );
 
-/* BANJO_EXPORT bj_byte_stream* bj_byte_stream_init_read( */
-/*     bj_byte_stream* p_instance, */
-/*     u8*               p_data, */
-/*     usize             length */
-/* ); */
+BANJO_EXPORT bj_byte_stream* bj_byte_stream_init_read(
+    bj_byte_stream* p_instance,
+    u8*             p_data,
+    usize           length
+);
+
+BANJO_EXPORT bj_byte_stream* bj_byte_stream_reset(
+    bj_byte_stream* p_instance
+);
+
+BANJO_EXPORT usize bj_byte_stream_read_byte(
+    bj_byte_stream* p_instance,
+    usize           count,
+    u8*             p_buffer
+);
+
+#define bj_byte_stream_read(stream, type, buffer) bj_byte_stream_read_byte(stream, sizeof(type), buffer)
+
+
 
 
--- src/byte_stream.c
+++ src/byte_stream.c
@@ -1,9 +1,47 @@
 #include <banjo/byte_stream.h>
+#include <banjo/memory.h>
 
 bj_byte_stream* bj_byte_stream_init_default(
     bj_byte_stream* p_stream,
     usize size
 ) {
+    bj_memset(p_stream, 0, sizeof(bj_byte_stream));
+    if(size > 0) {
+        p_stream->p_data = bj_malloc(p_stream->len = size);
+    }
     return p_stream;
+}
+
+bj_byte_stream* bj_byte_stream_init_read(
+    bj_byte_stream* p_stream,
+    u8*             p_data,
+    usize           length
+){
+    bj_memset(p_stream, 0, sizeof(bj_byte_stream));
+    if(p_data != 0 && length > 0) {
+        p_stream->p_data = p_data;
+        p_stream->weak   = true;
+        p_stream->len    = length;
+    }
+    return p_stream;
+}
+
+bj_byte_stream* bj_byte_stream_reset(
+    bj_byte_stream* p_stream
+) {
+    bj_assert(p_stream != 0);
+    if(!p_stream->weak && p_stream->p_data != 0) {
+        bj_free(p_stream->p_data);
+    }
+    bj_memset(p_stream, 0, sizeof(bj_byte_stream));
+    return p_stream;
+}
+
+usize bj_byte_stream_read_byte(
+    bj_byte_stream* p_instance,
+    usize           count,
+    u8*             p_buffer
+) {
+    return 0;
 }
 
