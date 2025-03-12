f4f137992578c3aabe60d52f996b774395d3a603
Kevin D'ORANGE <kevin.dorange@protonmail.com>
2025-03-12 11:41:35 +0100

    Squashed commit of the following:
    commit 0bdc93dbb7b7cf5ecddef4d0c3862d3f2edb194d
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Wed Mar 12 11:38:05 2025 +0100
    
        Base for Win32 windows
    
    commit 8e6979108e15dfabb9332f684ccac4aa0c0eb975
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Wed Mar 12 01:39:42 2025 +0100
    
        Some progress on WIN32
    
    commit 10ffdf64a5c49b83496fbfcd7def61ed8281e686
    Author: Kevin d'Orange <kdorange@techsoft3d.com>
    Date:   Tue Mar 11 08:22:38 2025 +0100
    
        Still not
    
    commit 18f5aa334520d9a36dbd9e2b40cf28c5eb6b202d
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Tue Mar 11 07:21:18 2025 +0100
    
        doesn't compile
    
    commit 6d742888178ccee0466bc0daa753e619088cdb4c
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Mon Mar 10 17:34:00 2025 +0100
    
        remove settings.json
    
    commit e803df671ca2b6bc77d577f10aab225d4cc6ce9a
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Mon Mar 10 17:32:56 2025 +0100
    
        Continue with presets
    
    commit abbbea63b4eee1c0fab15255d95b93ce603174b0
    Author: Kevin d'Orange <kevin.dorange@protonmail.com>
    Date:   Mon Mar 10 16:16:31 2025 +0100
    
        .
    
    commit 80533e8e42b3ee5960fa2e6d5976c3cc069726ba
    Author: Kevin d'Orange <kdorange@techsoft3d.com>
    Date:   Mon Mar 10 13:48:14 2025 +0100
    
        Backend loop
    
    commit 5eb1aade36979cfe792154e29e2068c9d4afe116
    Author: Kevin D'ORANGE <kevin.dorange@protonmail.com>
    Date:   Mon Mar 10 11:17:38 2025 +0100
    
        Create os.h file

--- src/system.c
+++ src/system.c
@@ -5,15 +5,21 @@
 #include "system_t.h"
 
 extern bj_system_backend_create_info fake_backend_create_info;
-#ifdef BANJO_FEATURE_X11
+#ifdef BJ_FEATURE_X11
 extern bj_system_backend_create_info x11_backend_create_info;
+#endif
+#ifdef BJ_FEATURE_WIN32
+extern bj_system_backend_create_info win32_backend_create_info;
 #endif
 
 static const bj_system_backend_create_info* backend_create_infos[] = {
-    &fake_backend_create_info,
-#ifdef BANJO_FEATURE_X11
+#ifdef BJ_FEATURE_WIN32
+    &win32_backend_create_info,
+#endif
+#ifdef BJ_FEATURE_X11
     &x11_backend_create_info,
 #endif
+    &fake_backend_create_info,
 };
 
 bj_system_backend* s_backend = 0;
@@ -23,14 +29,33 @@ bj_system_backend* s_backend = 0;
 bool bj_system_init(
     bj_error** p_error
 ) {
-    // TODO Smarter Backend selection
-    // For now, the backend is forced by taking the first available.
-    // What I want is the possibility to test each backend initialization in
-    // order of favorited, and return on the first that worked.
-    const bj_system_backend_create_info* p_create_info = backend_create_infos[FORCED_BACKEND];
-    s_backend = p_create_info->create(p_error);
-    bj_info("Initialized %s system backend", p_create_info->name);
-    return true;
+    const size_t n_backends = sizeof(backend_create_infos) / sizeof(bj_system_backend_create_info*);
+
+    for(size_t b = 0 ; b < n_backends ; ++b) {
+
+        bj_error* sub_err = 0;
+
+        const bj_system_backend_create_info* p_create_info = backend_create_infos[b];
+        bj_trace("Will try to initialize %s system backend", p_create_info->name);
+        bj_system_backend* p_backend = p_create_info->create(&sub_err);
+
+        if(sub_err) {
+            bj_message(p_backend == 0 ? 0 : 1, 0, 0,
+                "Error while initializing %s: %s (code 0x%08X)",
+                p_create_info->name, sub_err->message, sub_err->code
+            );
+            bj_clear_error(&sub_err);
+        }
+
+        if(p_backend != 0) {
+            bj_info("Initialized %s system backend", p_create_info->name);
+            s_backend = p_backend;
+            return true;
+        }
+    }
+
+    bj_set_error(p_error, BJ_ERROR_INITIALIZE, "No suitable system backend found");
+    return false;
 }
 
 void bj_system_dispose(
--- src/system_win32.c
+++ src/system_win32.c
@@ -1,16 +1,25 @@
-#pragma once
+#include <banjo/system.h>
 
 void* bj_load_library(
     const char* p_path
-);
+) {
+    (void)p_path;
+
+    return 0;
+}
 
 void bj_unload_library(
     void* p_handle
-);
+) {
+    (void)p_handle;
+}
 
 void* bj_get_symbol(
     void*       p_handle,
     const char* p_name
-);
+) {
+    (void)p_handle;
+    (void)p_name;
 
-
+    return 0;
+}
