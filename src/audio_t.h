ee2e1e229629e279946c10b2ab84eaed38f457f0
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-05-19 18:15:31 +0200

    .

--- src/audio_t.h
+++ src/audio_t.h
@@ -2,10 +2,17 @@
 
 #include <banjo/audio.h>
 
+struct bj_audio_stream_t {
+    int a;
+};
+
 typedef struct bj_audio_device_data_t bj_audio_device_data;
 
 struct bj_audio_device_t {
     unsigned int channels;
     unsigned int sample_rate;
+    struct bj_audio_stream_t stream;
     bj_audio_device_data* data;
 };
+
+
