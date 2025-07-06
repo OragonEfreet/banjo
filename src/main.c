6dcc9d54eab7f12ad0ee5dd89e307e46727e52e1
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-07-06 23:04:54 +0200

    source file for callbacks

--- src/main.c
+++ src/main.c
@@ -7,17 +7,3 @@ int bj_run_app(int argc, char* argv[], bj_main_fn_t function) {
 }
 #endif
 
-// Todo remove later
-int bj_enter_app_main_callbacks(
-    int argc, char* argv[],
-    bj_app_begin_fn_t app_begin,
-    bj_app_iterate_fn_t app_iterate,
-    bj_app_end_fn_t app_end
-) {
-    void* user_data = 0;
-    int status = app_begin(&user_data, argc, argv);
-    while (status > 0) {
-        status = app_iterate(user_data);
-    }
-    return app_end(user_data, status);
-}
