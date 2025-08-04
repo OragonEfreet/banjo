a90d021a9a5f8c209fde665d2f7c2d82c33be89a
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-08-04 13:54:02 +0200

    back to callbacks

--- examples/pong.c
+++ examples/pong.c
@@ -2,6 +2,7 @@
 /// \example pong.c
 /// A clone of Pong.
 ////////////////////////////////////////////////////////////////////////////////
+#define BJ_AUTOMAIN_CALLBACKS
 #include <banjo/bitmap.h>
 #include <banjo/event.h>
 #include <banjo/linmath.h>
@@ -48,6 +49,10 @@ static struct {
     },
     .running = BJ_FALSE,
 };
+
+bj_window* window      = 0;
+bj_bitmap* framebuffer = 0;
+bj_stopwatch stopwatch = {0};
 
 ////////////////////////////////////////////////////////////////////////////////
 // Draw the scene
@@ -131,29 +136,43 @@ void update(double delta_time) {
     }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char* argv[]) {
-    (void)argc; (void)argv;
+int bj_app_begin(void** user_data, int argc, char* argv[]) {
+    (void)user_data; (void)argc; (void)argv;
 
-    bj_begin(NULL);
+    bj_error* p_error = 0;
 
-    bj_window* window = bj_window_new("Pong", 0,0, SCREEN_W, SCREEN_H, 0);
-    bj_bitmap* framebuffer = bj_window_get_framebuffer(window, 0);
+    if(!bj_begin(&p_error)) {
+        bj_err("Error 0x%08X: %s", p_error->code, p_error->message);
+        return bj_callback_exit_error;
+    } 
+
+    window      = bj_window_new("Pong", 0,0, SCREEN_W, SCREEN_H, 0);
+    framebuffer = bj_window_get_framebuffer(window, 0);
 
     bj_set_key_callback(key_callback);
-    bj_stopwatch stopwatch = {0};
 
-    while (!bj_window_should_close(window)) {
-        bj_dispatch_events();
-        update(bj_stopwatch_step_delay(&stopwatch));
+    return bj_callback_continue;
+}
 
-        draw(framebuffer);
-        bj_window_update_framebuffer(window);
-        bj_sleep(15);
-    }
+int bj_app_iterate(void* user_data) {
+    (void)user_data;
 
+    bj_dispatch_events();
+    update(bj_stopwatch_step_delay(&stopwatch));
+
+    draw(framebuffer);
+    bj_window_update_framebuffer(window);
+    bj_sleep(15);
+
+    return bj_window_should_close(window) 
+         ? bj_callback_exit_success 
+         : bj_callback_continue;
+}
+
+int bj_app_end(void* user_data, int status) {
+    (void)user_data;
     bj_window_del(window);
     bj_end(0);
-    return 0;
+    return status;
 }
 
