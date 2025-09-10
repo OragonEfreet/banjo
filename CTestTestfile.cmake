6a867bd5bcd9eb4b4f632291f15b151a37b382f3
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-09-10 09:19:09 +0200

    Remove CMake Files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oragon/ws/banjo
-# Build directory: /home/oragon/ws/banjo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("test")
