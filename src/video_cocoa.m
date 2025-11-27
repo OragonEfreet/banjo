5ece0ea82fa7777e18aef484f4a200ef3db1bd3c
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-11-27 23:24:52 +0100

    2.2 - Basic init

--- src/video_cocoa.m
+++ src/video_cocoa.m
@@ -21,8 +21,26 @@ static bj_video_layer* cocoa_init_video(
     bj_error** p_error
 ) {
     (void)p_error;
+    @autoreleasepool {
+        NSApplication* app = [NSApplication sharedApplication];
+        [app setActivationPolicy:NSApplicationActivationPolicyRegular];
 
-    return 0;
+        cocoa* p_cocoa = bj_malloc(sizeof(cocoa));
+        p_cocoa->app = app;
+
+        bj_video_layer* p_layer = bj_malloc(sizeof(bj_video_layer));
+        p_layer->data                      = p_cocoa;
+        p_layer->end                       = 0;
+        p_layer->create_window             = 0;
+        p_layer->delete_window             = 0;
+        p_layer->poll_events               = 0;
+        p_layer->get_window_size           = 0;
+        p_layer->create_window_framebuffer = 0;
+        p_layer->flush_window_framebuffer  = 0;
+
+        bj_info("Cocoa backend initialized");
+        return p_layer;
+    }
 }
 
 bj_video_layer_create_info cocoa_video_layer_info = {
