ec222de71fa1e1e2458786adb76350521c565779
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-11-27 23:35:21 +0100

    Define Window Structure

--- src/video_cocoa.m
+++ src/video_cocoa.m
@@ -11,11 +11,40 @@
 #include <banjo/video.h>
 #include <banjo/window.h>
 
+#include "window_t.h"
+
 #import <Cocoa/Cocoa.h>
 
 typedef struct bj_video_layer_data_t {
     NSApplication* app;
 } cocoa;
+
+@interface BanjoView : NSView
+@end
+
+@implementation BanjoView
+- (BOOL)acceptsFirstResponder {
+    return YES;
+}
+@end
+
+typedef struct {
+    struct bj_window_t common;
+    NSWindow*          window;
+    BanjoView*         view;
+} cocoa_window;
+
+static void cocoa_end_video(
+    bj_video_layer* p_video,
+    bj_error** p_error
+) {
+    (void)p_error;
+
+    cocoa* p_cocoa = (cocoa*)p_video->data;
+    bj_free(p_cocoa);
+    bj_free(p_video);
+    bj_info("Cocoa backend terminated");
+}
 
 static bj_video_layer* cocoa_init_video(
     bj_error** p_error
@@ -30,7 +59,7 @@ static bj_video_layer* cocoa_init_video(
 
         bj_video_layer* p_layer = bj_malloc(sizeof(bj_video_layer));
         p_layer->data                      = p_cocoa;
-        p_layer->end                       = 0;
+        p_layer->end                       = cocoa_end_video;
         p_layer->create_window             = 0;
         p_layer->delete_window             = 0;
         p_layer->poll_events               = 0;
