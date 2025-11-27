00662bc805a3d7f62afe7a2c80016d4f1612ddf1
Kevin d'Orange <kevin.dorange@protonmail.com>
2025-11-27 23:44:23 +0100

    Create window

--- src/video_cocoa.m
+++ src/video_cocoa.m
@@ -34,6 +34,52 @@ typedef struct {
     BanjoView*         view;
 } cocoa_window;
 
+static bj_window* cocoa_create_window(
+    bj_video_layer* p_video,
+    const char* p_title,
+    uint16_t x,
+    uint16_t y,
+    uint16_t width,
+    uint16_t height,
+    uint8_t  flags
+) {
+    @autoreleasepool {
+        /* cocoa* p_cocoa = p_video->data; */
+
+        // Flip Y coordinate (Cocoa uses bottom-left origin)
+        NSScreen* screen = [NSScreen mainScreen];
+        NSRect screenFrame = [screen frame];
+        CGFloat flippedY = screenFrame.size.height - y - height;
+
+        // Create window
+        NSRect contentRect = NSMakeRect(x, flippedY, width, height);
+        NSUInteger styleMask = NSWindowStyleMaskTitled
+                             | NSWindowStyleMaskClosable
+                             | NSWindowStyleMaskMiniaturizable;
+
+        NSWindow* nsWindow = [[NSWindow alloc]
+            initWithContentRect:contentRect
+                      styleMask:styleMask
+                        backing:NSBackingStoreBuffered
+                          defer:NO];
+
+        [nsWindow setTitle:[NSString stringWithUTF8String:p_title]];
+
+        BanjoView* view = [[BanjoView alloc] initWithFrame:contentRect];
+        [nsWindow setContentView:view];
+
+        cocoa_window* window = bj_malloc(sizeof(cocoa_window));
+        window->common.flags = flags;
+        window->window       = nsWindow;
+        window->view         = view;
+
+        [nsWindow makeKeyAndOrderFront:nil];
+
+        bj_info("Cocoa window created: %dx%d", width, height);
+        return (bj_window*)window;
+    }
+}
+
 static void cocoa_end_video(
     bj_video_layer* p_video,
     bj_error** p_error
@@ -60,7 +106,7 @@ static bj_video_layer* cocoa_init_video(
         bj_video_layer* p_layer = bj_malloc(sizeof(bj_video_layer));
         p_layer->data                      = p_cocoa;
         p_layer->end                       = cocoa_end_video;
-        p_layer->create_window             = 0;
+        p_layer->create_window             = cocoa_create_window;
         p_layer->delete_window             = 0;
         p_layer->poll_events               = 0;
         p_layer->get_window_size           = 0;
