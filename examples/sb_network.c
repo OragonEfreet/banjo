1cde501df1da4e6ebcb6c62c5a01a58749b009cc
Kevin d'Orange <kevin.dorange@protonmail.com>
2026-02-05 23:07:58 +0100

    .

--- examples/sb_network.c
+++ examples/sb_network.c
@@ -28,12 +28,32 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    printf("IP addresses for %s:\n\n", argv[1]);
 
-     
+    for(p = res ; p != NULL ; p = p->ai_next) {
+        void* addr;
+        char *ipver;
+        struct sockaddr_in* ipv4;
+        struct sockaddr_in6* ipv6;
 
+        // get the pointer to the address ifself
+        // differend fields in IPv4 and IPv6
+        if(p->ai_family == AF_INET) {
+            ipv4 = (struct sockaddr_in*)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            ipv6 = (struct sockaddr_in6*)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
 
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("\t%s: %s\n", ipver, ipstr);
+    }
 
-
+    freeaddrinfo(res); // free the linked list
     return 0;
 }
 
